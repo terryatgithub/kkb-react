ad211ab7d3e39afe7570b3161ac3cff82da17f21
yuanbo <yuanbo@coocaa.com>
2020-08-07 22:58:55 +0800

    react ls5 practice

--- lesson4/src/k-react-router-dom/Prompt.js
+++ lesson4/src/k-react-router-dom/Prompt.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { RouterContext } from "./Context";
 
-export default function Prompt(props) {
+export default function Prompt({ message, when = true }) {
   return (
-    <div>
-      <h3>Prompt</h3>
-    </div>
+    <RouterContext.Consumer>
+      {
+        context => {
+          if (!when) {
+            return null
+          }
+          let method = context.history.block
+          console.log('method', method)
+          // @暗号 尼日尔
+          return (
+            <LifeCycle
+              onMount={self => {
+                //@助教老师，这里的self.release能讲解下吗，谢谢。
+                self.release = method(message)
+              }}
+              onUnmount={self => {
+                self.release()
+              }}
+              message={message}
+            >
+            </LifeCycle>
+          )
+        }
+      }
+    </RouterContext.Consumer>
   );
+}
+
+class LifeCycle extends React.Component {
+  componentDidMount() {
+    if (this.props.onMount) {
+      this.props.onMount.call(this, this)
+    }
+  }
+  componentWillUnmount() {
+    if (this.props.onUnmount) {
+      this.props.onUnmount.call(this, this)
+    }
+  }
+
+  render() {
+    return null;
+  }
+
 }
--- lesson4/src/pages/HomePage.js
+++ lesson4/src/pages/HomePage.js
@@ -4,13 +4,13 @@ import {Redirect} from "../k-react-router-dom/";
 export default class HomePage extends Component {
   render() {
     console.log("props", this.props); //sy-log
-    return (
-      <Redirect
-        to={{
-          pathname: "/welcome"
-        }}
-      />
-    );
+    // return (
+    //   <Redirect
+    //     to={{
+    //       pathname: "/welcome"
+    //     }}
+    //   />
+    // );
     return (
       <div>
         <h3>HomePage</h3>
